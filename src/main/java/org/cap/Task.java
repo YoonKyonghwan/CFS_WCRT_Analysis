dd4d13a3efa42417e997f75d103fda8ea0e544bb
Jonghyun Choe <andy.choe729@gmail.com>
2023-07-04 21:42:05 +0900

    Add task

---
+++ src/main/java/org/cap/Task.java
@@ -0,0 +1,12 @@
+package org.cap;
+
+import java.util.LinkedList;
+
+public class Task {
+    int startTime;
+    int worstCaseExecutionTime;
+    int nice;
+    int period;
+    int virtualRuntime;
+    LinkedList<Integer> responseTimes = new LinkedList<>();
+}
