f6cbb38986a95e54f70e337ced66e16c39f387c4
Jonghyun Choe <andy.choe729@gmail.com>
2023-07-27 11:34:27 +0900

    Remove unused code from simulation state

--- src/main/java/org/cap/SimulationState.java
+++ src/main/java/org/cap/SimulationState.java
@@ -4,18 +4,9 @@ public class SimulationState {
     BlockingPolicy blockingPolicy;
     String writingTaskKey;
 
-    public SimulationState copy() {
-        SimulationState newSimulationState = new SimulationState();
-        newSimulationState.blockingPolicy = this.blockingPolicy;
-        newSimulationState.writingTaskKey = this.writingTaskKey;
-        return newSimulationState;
-    }
-
     public SimulationState(BlockingPolicy blockingPolicy, String writingTaskKey) {
         this.blockingPolicy = blockingPolicy;
         this.writingTaskKey = writingTaskKey;
     }
 
-    public SimulationState() {
-    }
 }
