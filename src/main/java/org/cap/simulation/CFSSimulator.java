42f1faf0cd1cbad034eb8b2852654c880fd184d1
Jonghyun Choe <andy.choe729@gmail.com>
2023-08-18 14:19:30 +0900

    Divide into read, body, write stage

--- src/main/java/org/cap/simulation/CFSSimulator.java
+++ src/main/java/org/cap/simulation/CFSSimulator.java
@@ -11,7 +11,6 @@ public class CFSSimulator {
     private static final Logger logger = LoggerUtility.getLogger();
 
     // TODO refactor to simulation state later
-    // TODO remainingRuntime
     boolean isReadBlocked = false;
     boolean isWriteBlocked = false;
     Task currentTask = null;
@@ -30,40 +29,86 @@ public class CFSSimulator {
         // TODO consider using red-black trees
         List<Queue<Task>> queues = initializeQueues(cores);
 
-        performSimulation(cores, queues, targetedLatency, minimumGranularity, time);
+        performSimulation(cores, queues, time);
 
         LoggerUtility.addConsoleLogger();
         return new SimulationResult(schedulability);
     }
 
-    private void performSimulation(List<Core> cores, List<Queue<Task>> queues, int targetedLatency, int minimumGranularity, int time) {
+    private void performSimulation(List<Core> cores, List<Queue<Task>> queues, int time) {
         int hyperperiod = MathUtility.getLCM(cores);
-
-        // TODO apply read, body, write
 
         while (time < hyperperiod) {
             for (int i = 0; i < cores.size(); i++) {
+                // TODO add periodic jobs
                 Queue<Task> queue = queues.get(i);
                 Task task = selectTask(queue); // selectTask 함수 안에 minimum virtual runtime 로직 존재
                 if (task == null)
                     continue;
 
-                task.bodyTime--;
-                if (task.bodyTime <= 0) {
-                    task.WCRT_by_CFS_simulator = time + 1 - task.startTime;
-                    isRunning = false;
-                    remainingRuntime = 0;
-                }
-                else {
-                    remainingRuntime--;
-                    if (remainingRuntime <= 0)
-                        isRunning = false;
-                    // TODO add task back to queue and reschedule after modifying virtual runtime
-                }
-                // The vruntime variable is updated after each run of the process.
-                task.virtualRuntime += 1024/task.weight;
+                executeTask(task, queue, time);
             }
             time++;
+        }
+    }
+
+    private void executeTask(Task task, Queue<Task> queueInCore, int time) {
+        logger.info("Task " + task.id + " executed for 1 in stage: " + task.stage);
+
+        // Decrease runtime
+        remainingRuntime--;
+        if (remainingRuntime <= 0)
+            isRunning = false;
+
+        // Update virtual runtime
+        task.virtualRuntime += 1024/ task.weight;
+
+        // Decrease execution time for each stage
+        switch (task.stage) {
+            case READ:
+                task.readTime--;
+                if (task.readTime <= 0) {
+                    task.stage = Stage.BODY;
+                    task.bodyReleaseTime = time + 1;
+                }
+                else {
+                    if (isRunning)
+                        isReadBlocked = true;
+                }
+                break;
+            case BODY:
+                task.bodyTime--;
+                if (task.bodyTime <= 0) {
+                    if (task.writeTime > 0) {
+                        task.stage = Stage.WRITE;
+                        task.writeReleaseTime = time + 1;
+                    }
+                    else {
+                        task.stage = Stage.COMPLETED;
+                        isRunning = false;
+                    }
+                }
+                break;
+            case WRITE:
+                task.writeTime--;
+                if (task.writeTime <= 0) {
+                    task.stage = Stage.COMPLETED;
+                    isRunning = false;
+                }
+                else
+                    isWriteBlocked = true;
+                break;
+        }
+
+        // Add task back to queue if task is not finished but runtime is over
+        if (task.stage != Stage.COMPLETED) {
+            if (isRunning)
+                currentTask = task;
+            else
+                queueInCore.add(task);
+        } else {
+            logger.info("Task " + task.id + " completed at time " + (time + 1) + " with RT " + (time - task.readReleaseTime + 1));
+            task.WCRT_by_CFS_simulator = time + 1 - task.readReleaseTime;
         }
     }
 
@@ -81,22 +126,20 @@ public class CFSSimulator {
         return queues;
     }
 
-    private Task selectTask(Queue<Task> queue) {
+    private Task selectTask(Queue<Task> queueInCore) {
         // Case 1: running task already exists
-        // Use a flag
         if (isRunning) {
             return currentTask;
         }
         // Case 2: select a new task if a new task is released
-        // Choose task with minimum virtual runtime
-        // runtime with following formula: runtime = max(targetedLatency * (task.weight / totalWeight), minimumGranularity)
+        // TODO consider blocking policy
         else {
             // TODO check if diverging is needed
-            Task task = queue.poll();
+            Task task = queueInCore.poll();
             if (task == null)
                 return null;
 
-            double totalWeight = queue.stream().mapToDouble(t -> t.weight).sum();
+            double totalWeight = queueInCore.stream().mapToDouble(t -> t.weight).sum();
             remainingRuntime = (int) Math.max(targetedLatency * task.weight / totalWeight, minimumGranularity);
             isRunning = true;
             return task;
