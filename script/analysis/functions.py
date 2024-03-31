b967bd71d2cbdd99e7a2f962fc5911df80e8d82d
Yoon Kyonghwan <ykw6644@gmail.com>
2024-04-01 01:36:38 +0900

    update exp_results & minor revision

--- script/analysis/functions.py
+++ script/analysis/functions.py
@@ -180,7 +180,7 @@ def show_box_plot(file_path, values, title, fontsize, xtick_rotation):
         labels.append(f"nT: {numTasks}, U: {utilization}")
 
     # Create the box plot
-    plt.figure(figsize=(15, 8))
+    plt.figure(figsize=(12, 8))
     # plt.boxplot(boxplot_data, labels=labels)
     plt.boxplot(boxplot_data, labels=labels, showfliers=False)
     plt.xticks(rotation=xtick_rotation)
