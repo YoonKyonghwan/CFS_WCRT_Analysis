62f6e96e5a72d659986a8961c82d5809656b2c35
Yoon Kyonghwan <ykw6644@gmail.com>
2024-04-03 17:10:25 +0900

    polish script for simulator and proposed method

--- script/run/proposed_and_simulator/run_exps.sh
+++ script/run/proposed_and_simulator/run_exps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # set arguments
-result_dir="exp_results_test_20240328_30ms_final_5"
+taskset_dir="./generated_taskset_final"
+result_dir="./exp_results_proposed_and_simulator"
 
-generated_files_save_dir="generated_taskset_new_100_20240328_30ms"
 num_cores=(1)
 num_tasks=(3 6 9 12)
 utilizations=(0.4 0.6 0.8)
@@ -32,8 +32,8 @@ for num_core in "${num_cores[@]}"; do
         for utilization in "${utilizations[@]}"; do
             for ((i=0; i<num_sets; i++)); do
                 file_name="${num_core}cores_${num_task}tasks_${utilization}utilization_${i}.json"
-                task_info_path="${generated_files_save_dir}/${num_core}cores/${num_task}tasks/${utilization}utilization/${file_name}"
-                cmd="java -jar run.jar -t=$task_info_path -rd=$result_dir -ssm=$schedule_simulation_method -stc=$schedule_try_count -ttc=$test_try_count -tl=$target_latency -mg=$min_gran -jf=$jiffy_us -lo=info -nl=$nice_lambda"
+                task_info_path="${taskset_dir}/${num_core}cores/${num_task}tasks/${utilization}utilization/${file_name}"
+                cmd="java -jar run.jar -t=$task_info_path -rd=$result_dir -ssm=$schedule_simulation_method -stc=$schedule_try_count -ttc=$test_try_count -tl=$target_latency -mg=$min_gran -jf=$jiffy_us -nl=$nice_lambda -lo=info"
                 echo $cmd
                 ${cmd}
             done
